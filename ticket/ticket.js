907767dbffd8eaf17b4381e94228e38496950a99
TechLuque <159042787+TechLuque@users.noreply.github.com>
2025-05-21 11:48:32 -0500

    upd

--- ticket/ticket.js
+++ ticket/ticket.js
@@ -1,4 +1,30 @@
-    const speed = 7;
+const URL_API = "https://script.google.com/macros/s/AKfycbysE2U4FStVMSAqoexL_D057mTdg_46P2vzR6_mpsHke6icpdrHzjqamUauFJqtYhUZ9Q/exec";
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const labelNombre = document.querySelector(".name"); // El nombre grande en la tarjeta
+
+  const correo = localStorage.getItem("correo");
+  if (!correo) return;
+
+  try {
+    const res = await fetch(URL_API);
+    const data = await res.json();
+
+    const match = data.email.find(item => item.email.trim().toLowerCase() === correo.trim().toLowerCase());
+
+    if (match) {
+      labelNombre.textContent = match.name.toUpperCase();
+    } else {
+      labelNombre.textContent = "USUARIO NO ENCONTRADO";
+    }
+  } catch (err) {
+    console.error("Error al cargar los datos:", err);
+  }
+});
+
+
+
+const speed = 7;
 const r = gsap.timeline({ repeat: -1 });
 const o = gsap.timeline({ repeat: -1 });
 const h = gsap.timeline({ repeat: -1 });
