44be26f24c689867bac4736fea4684b6a0be555d
TechLuque <159042787+TechLuque@users.noreply.github.com>
2025-05-25 15:00:45 -0500

    upd

--- auditorio/time/time.js
+++ auditorio/time/time.js
@@ -1,19 +1,19 @@
-function updateCountdown() {
-  const eventDate = new Date('2025-06-09T09:00:00').getTime(); // Fecha del evento
-  const now = new Date().getTime();
-  const timeLeft = eventDate - now;
+// function updateCountdown() {
+//   const eventDate = new Date('2025-06-09T09:00:00').getTime(); 
+//   const now = new Date().getTime();
+//   const timeLeft = eventDate - now;
 
-  const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-  const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+//   const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+//   const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+//   const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
-  // Actualiza el contenido HTML
-  document.getElementById('days').textContent = String(days).padStart(2, '0');
-  document.getElementById('hours').textContent = String(hours).padStart(2, '0');
-  document.getElementById('minutes').textContent = String(minutes).padStart(2, '0');
-  document.getElementById('seconds').textContent = String(seconds).padStart(2, '0');
-}
 
-setInterval(updateCountdown, 1000); // Ejecuta cada segundo
-updateCountdown(); // Llama una vez al principio
+//   document.getElementById('days').textContent = String(days).padStart(2, '0');
+//   document.getElementById('hours').textContent = String(hours).padStart(2, '0');
+//   document.getElementById('minutes').textContent = String(minutes).padStart(2, '0');
+//   document.getElementById('seconds').textContent = String(seconds).padStart(2, '0');
+// }
+
+// setInterval(updateCountdown, 1000); 
+// updateCountdown(); 
